cd0a3a8bb47daff5bcbfe22d3c86b9eea8c3c52e
yh <>
2023-01-17 20:58:53 +0900

    [add]ユーザー登録処理の追加(バリテーションやセキュリティ面の考慮なし)

--- match(フルスクラッチ)/private/signUpFunction.php
+++ match(フルスクラッチ)/private/signUpFunction.php
@@ -13,20 +13,7 @@
   $email = $_POST['email'];
   $password = $_POST['password'];
 
-  debug('送信判定に入ります');
-  if(!empty($email) && !empty($password)){
-    debug('接続・登録処理に入ります');
-    try {
-      $pdo = new PDO(DSN, DB_USERNAME, DB_PASSWORD);
-      $stmt = $pdo->prepare('INSERT INTO users  (email,password,create_date) VALUES(:email,:pass,:create_date)',
-      array(':email' => $email,':pass' => $password,':create_date' => date('Y-m-d H:i:s')));
-
-      $stmt->execute(array(':email' => $email));
-      $user = $stmt->fetch();
-    } catch (Exception $e) {
-      error_log('エラー発生:' . $e->getMessage());
-    }
-  }
+  debug('バリテーション処理に入ります');
 
   // debug('');
   // // ユーザーが見つからなかった場合
@@ -42,6 +29,22 @@
   //     header('Location: ../index.php');
   //     exit;
   // }
+
+  debug('送信判定に入ります');
+  if(!empty($email) && !empty($password)){
+    debug('接続・登録処理に入ります');
+    try {
+      $pdo = new PDO(DSN, DB_USERNAME, DB_PASSWORD);
+      $stmt = $pdo->prepare('INSERT INTO users  (email,password,create_date) VALUES(:email,:pass,:create_date)',
+      array(':email' => $email,':pass' => $password,':create_date' => date('Y-m-d H:i:s')));
+      $stmt->execute(array(':email' => $email,':pass' => $password,':create_date' => date('Y-m-d H:i:s')));
+      $user = $stmt->fetch();
+    } catch (Exception $e) {
+      error_log('エラー発生:' . $e->getMessage());
+    }
+  }
+
+  debug('登録が完了しました。');
 
   // $_SESSION['logged_in'] = true;
   // $_SESSION['user_id'] = $user['id'];
