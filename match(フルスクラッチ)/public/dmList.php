c5e4a5d48cf1f8688fef89eb38cc792e4057f447
yh <>
2023-01-31 22:25:09 +0900

    [wip]dmリストの処理記述

---
+++ match(フルスクラッチ)/public/dmList.php
@@ -0,0 +1,107 @@
+<!-- まだ途中 -->
+<!-- <?php
+
+//共通変数・関数ファイルを読込み
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('「　連絡掲示板ページ　');
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debugLogStart();
+
+//================================
+// 画面処理
+//================================
+$partnerUserId = '';
+$partnerUserInfo = '';
+$myUserInfo = '';
+$productInfo = '';
+// 画面表示用データ取得
+//================================
+// GETパラメータを取得
+$m_id = (!empty($_GET['m_id'])) ? $_GET['m_id'] : '';
+// DBから掲示板とメッセージデータを取得
+$viewData = getMsgsAndBord($m_id);
+debug('取得したDBデータ：'.print_r($viewData,true));
+// パラメータに不正な値が入っているかチェック
+if(empty($viewData)){
+  error_log('エラー発生:指定ページに不正な値が入りました');
+  header("Location:mypage.php"); //マイページへ
+}
+// 商品情報を取得
+$productInfo = getProductOne($viewData[0]['product_id']);
+
+debug('取得したDBデータ：'.print_r($productInfo,true));
+// 商品情報が入っているかチェック
+if(empty($productInfo)){
+  error_log('エラー発生:商品情報が取得できませんでした');
+  header("Location:mypage.php"); //マイページへ
+}
+// viewDataから相手のユーザーIDを取り出す
+$dealUserIds[] = $viewData[0]['sale_user'];
+$dealUserIds[] = $viewData[0]['buy_user'];
+if(($key = array_search($_SESSION['user_id'], $dealUserIds)) !== false) {
+    unset($dealUserIds[$key]);
+}
+$partnerUserId = array_shift($dealUserIds);
+debug('取得した相手のユーザーID：'.$partnerUserId);
+// DBから取引相手のユーザー情報を取得
+if(isset($partnerUserId)){
+  $partnerUserInfo = getUser($partnerUserId);
+}
+// 相手のユーザー情報が取れたかチェック
+if(empty($partnerUserInfo)){
+  error_log('エラー発生:相手のユーザー情報が取得できませんでした');
+  header("Location:mypage.php"); //マイページへ
+}
+// DBから自分のユーザー情報を取得
+$myUserInfo = getUser($_SESSION['user_id']);
+debug('取得したユーザデータ：'.print_r($partnerUserInfo,true));
+// 自分のユーザー情報が取れたかチェック
+if(empty($myUserInfo)){
+  error_log('エラー発生:自分のユーザー情報が取得できませんでした');
+  header("Location:mypage.php"); //マイページへ
+}
+
+// post送信されていた場合
+if(!empty($_POST)){
+  debug('POST送信があります。');
+
+  //ログイン認証
+  require('auth.php');
+
+  //バリデーションチェック
+  $msg = (isset($_POST['msg'])) ? $_POST['msg'] : '';
+  //最大文字数チェック
+  validMaxLen($msg, 'msg', 500);
+  //未入力チェック
+  validRequired($msg, 'msg');
+
+  if(empty($err_msg)){
+    debug('バリデーションOKです。');
+
+    //例外処理
+    try {
+      // DBへ接続
+      $dbh = dbConnect();
+      // SQL文作成
+      $sql = 'INSERT INTO message (bord_id, send_date, to_user, from_user, msg, create_date) VALUES (:b_id, :send_date, :to_user, :from_user, :msg, :date)';
+      $data = array(':b_id' => $m_id, ':send_date' => date('Y-m-d H:i:s'), ':to_user' => $partnerUserId, ':from_user' => $_SESSION['user_id'], ':msg' => $msg, ':date' => date('Y-m-d H:i:s'));
+      // クエリ実行
+      $stmt = queryPost($dbh, $sql, $data);
+
+      // クエリ成功の場合
+      if($stmt){
+        $_POST = array(); //postをクリア
+        debug('連絡掲示板へ遷移します。');
+        header("Location: " . $_SERVER['PHP_SELF'] .'?m_id='.$m_id); //自分自身に遷移する
+      }
+
+    } catch (Exception $e) {
+      error_log('エラー発生:' . $e->getMessage());
+      $err_msg['common'] = MSG07;
+    }
+  }
+}
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+?> -->
